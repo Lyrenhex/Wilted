d2a2edb37b1f14a990c678990d701c880c84669f
Damian Heaton <dh64784@gmail.com>
2017-01-29 13:05:27 +0000

    Rewrite Wilted to use fs-based backend

---
+++ index.js
@@ -0,0 +1,105 @@
+/*
+No clue wtf happened, but websockets stopped behaving as they usually do, and nodejs killed the listener. This version works based on files.
+*/
+
+const fs = require('fs');
+const os = require('os');
+const request = require('request');
+var querystring = require('querystring');
+
+try{
+  var conf = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
+}catch(err){
+  conf = {
+    username: false,
+    password: false
+  }
+  console.log("Whoops, badly formatted JSON config. Please make sure that your username and password are both encased in quotes (\"s) and that you have not drastically changed the format of the file. Thanks!")
+}
+
+if(conf.username !== false && conf.password !== false){
+  var Auth = {
+    'token': '',
+    'drfHeader': ''
+  };
+
+  request.post(
+    `${conf.api}api-token-auth/`,
+    { json: {
+      'username': conf.username,
+      'password': conf.password
+    }},
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log('Connected to WILT API and logged in');
+        Auth.token = body.token;
+        Auth.drfHeader = `Token ${Auth.token}`;
+        check();
+      } else if(error) {
+          console.log(error);
+      } else {
+        console.log(response, body);
+      }
+    });
+} else {
+  console.log('Yo, you need to set your WILT.fm username and password in the config.json file.');
+  process.exit();
+}
+
+function check() {
+  var filloc;
+  switch (os.platform()){
+    case "linux":
+      filloc = `${process.env.HOME}/.config/Google Play Music Desktop Player/json_store/playback.json`;
+      break;
+    case "win32":
+      filloc = `${process.env.APPDATA}\\Google Play Music Desktop Player\\json_store\\playback.json`;
+      break;
+    case "darwin":
+      filloc = `${process.env.HOME}/Library/Application Support/Google Play Music Desktop Player/json_store/playback.json`;
+      break;
+  }
+  fs.readFile(filloc, (err, data) => {
+    if (err) {
+      console.log("Couldn't read the JSON data file: ", err);
+      setTimeout(check, 10000);
+    }else{
+      try {
+        var json = JSON.parse(data);
+
+        if(json.playing){
+          var nextCheck = json.time.total - (json.time.current - 1000);
+          setTimeout(check, nextCheck); // check once the song is over, and give a 1sec grace period for GPMDP
+          request({
+            headers: {
+              'Authorization': Auth.drfHeader
+            },
+            url: `${conf.api}scrobbles/`,
+            json: true,
+            body: {
+              'song': json.song.title,
+              'artist': json.song.artist,
+              'album': json.song.album
+            },
+            method: 'POST'
+          },
+          function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              console.log(`Scrobbled ${json.song.title} by ${json.song.artist} on the album ${json.song.album}. Checking next in ${nextCheck / 1000} seconds.`);
+            } else if(error) {
+              console.log(error);
+            } else {
+              console.log(response, body);
+            }
+          });
+        }else {
+          setTimeout(check, 10000);
+        }
+      }catch(e){
+        console.log("Uhh... JSON data file empty?");
+        console.log(e);
+        setTimeout(check, 10000);
+      }
+    }
+  });
+}
--- main.js
+++ main.js
@@ -39,6 +39,7 @@ if(conf.username !== false && conf.password !== false){
     });
 } else {
   console.log('Yo, you need to set your WILT.fm username and password in the config.json file.');
+  process.exit();
 }
 
 function start() {
@@ -54,55 +55,51 @@ function start() {
     }
   });
 
-  wsClient.on('text', function (str) {
-    var json = JSON.parse(str);
-    if (json.channel == 'track') {
-      var song = json.payload;
-      if (song.title !== null) {
-        if (song.title.toLowerCase().indexOf('(explicit)') > -1) {
-          song.title = song.title.replace(/(explicit)/ig, '');
-          if (song.title.indexOf('()') > -1) {
-            song.title = song.title.replace('()', '');
-          }
-        }
-
-        var scrobbleData = {
-          song: song.title,
-          artist: song.artist,
-          album: song.album
-        }
-        var scrobbleSend = querystring.stringify(scrobbleData);
-        var contentLength = scrobbleSend.length;
-        request({
-          headers: {
-            'Authorization': Auth.drfHeader
-          },
-          url: `${conf.api}scrobbles/`,
-          json: true,
-          body: {
-            'song': song.title,
-            'artist': song.artist,
-            'album': song.album
-          },
-          method: 'POST'
-        },
-        function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            console.log(`Scrobbled ${song.title} by ${song.artist} on the album ${song.album}`);
-          } else if(error) {
-            console.log(error);
-          } else {
-            console.log(response, body);
-          }
-        });
-        // alright, at this point we should scrobble to WILT
-        // I'll need to look at https://github.com/ModalSeoul/Weeb-Chrome-Scrobbler/blob/master/WILT.js scrobble function to see how Modal scrobbles data
-        // sending post requests, for ref: http://stackoverflow.com/questions/6158933/how-to-make-an-http-post-request-in-node-js
-        }
-      }
-    });
-
     function connected() {
       console.log('Connected to websocket');
+      wsClient.on('text', function (str) {
+        var json = JSON.parse(str);
+        if (json.channel == 'track') {
+          var song = json.payload;
+          if (song.title !== null) {
+            if (song.title.toLowerCase().indexOf('(explicit)') > -1) {
+              song.title = song.title.replace(/(explicit)/ig, '');
+              if (song.title.indexOf('()') > -1) {
+                song.title = song.title.replace('()', '');
+              }
+            }
+
+            var scrobbleData = {
+              song: song.title,
+              artist: song.artist,
+              album: song.album
+            }
+            var scrobbleSend = querystring.stringify(scrobbleData);
+            var contentLength = scrobbleSend.length;
+            request({
+              headers: {
+                'Authorization': Auth.drfHeader
+              },
+              url: `${conf.api}scrobbles/`,
+              json: true,
+              body: {
+                'song': song.title,
+                'artist': song.artist,
+                'album': song.album
+              },
+              method: 'POST'
+            },
+            function (error, response, body) {
+              if (!error && response.statusCode == 200) {
+                console.log(`Scrobbled ${song.title} by ${song.artist} on the album ${song.album}`);
+              } else if(error) {
+                console.log(error);
+              } else {
+                console.log(response, body);
+              }
+            });
+            }
+          }
+        });
     }
 }
